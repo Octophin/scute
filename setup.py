0572bd27480a6415a57a8698a2e40c7156b6ea9e
FilipNest <filip@octophin.com>
2019-07-15 22:44:51 +0100

    Version 0.2.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='scute',
-      version='0.1.26',
+      version='0.2.0',
       description='Tool for making hardware user interfaces',
       url='https://github.com/Octophin/scute',
       author='Octophin Digital',
