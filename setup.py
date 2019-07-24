b7909e2ec920f01aacbd09777da9f2e61f1a4634
Cris Thompson <cris@octophin.co.uk>
2019-07-24 15:28:19 +0100

    update verion number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='scute',
-      version='0.2.3',
+      version='0.2.4',
       description='Tool for making hardware user interfaces',
       url='https://github.com/Octophin/scute',
       author='Octophin Digital',
