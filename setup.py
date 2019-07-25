f75eca236655365fc6a9873d159d4ed42181aebc
FilipNest <filip@octophin.com>
2019-07-25 14:31:22 +0100

    Make roboto font local

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='scute',
-      version='0.2.8',
+      version='0.2.9',
       description='Tool for making hardware user interfaces',
       url='https://github.com/Octophin/scute',
       author='Octophin Digital',
