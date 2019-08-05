1623eb98b9005c12554a3fc2971ca9b3f98cae12
Cris Thompson <cris@octophin.co.uk>
2019-08-05 18:34:32 +0100

    0.2.26

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='scute',
-      version='0.2.25',
+      version='0.2.26',
       description='Tool for making hardware user interfaces',
       url='https://github.com/Octophin/scute',
       author='Octophin Digital',
