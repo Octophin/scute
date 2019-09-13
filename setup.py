47d87310cac505c088f89862f63434401509eb33
Cris Thompson <cris@octophin.co.uk>
2019-09-13 10:57:44 +0100

    Updating to version 0.4.0 for tools support v0.0.7

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='scute',
-      version='0.3.1',
+      version='0.4.0',
       description='Tool for making hardware user interfaces',
       url='https://github.com/Octophin/scute',
       author='Octophin Digital',
