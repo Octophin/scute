bfb9016b30d14012267d4ec96c1628fa3006e99f
Cris Thompson <cris@octophin.co.uk>
2019-10-10 13:53:46 +0100

    adding datetimeformat to js

--- scute/client_side/scute.js
+++ scute/client_side/scute.js
@@ -267,3 +267,23 @@ function toggleBurger() {
     document.body.toggleAttribute("data-open");
 }
 toggleBurger();
+
+function formatDateTime(date) {
+    var d = new Date(date),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear(),
+        hour = d.getHours(),
+        minute = d.getMinutes();
+
+    if (month.length < 2) 
+        month = '0' + month;
+    if (day.length < 2) 
+        day = '0' + day;
+    if (hour.length < 2) 
+        hour = '0' + hour;
+    if (minute.length < 2) 
+        minute = '0' + minute;
+
+    return year + '-' + month + '-' +  day + ' ' + hour + ':' + minute;
+}
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='scute',
-      version='0.4.6',
+      version='0.4.7',
       description='Tool for making hardware user interfaces',
       url='https://github.com/Octophin/scute',
       author='Octophin Digital',
