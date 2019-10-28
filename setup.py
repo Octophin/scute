6fa99f2595015b8cf14fa3495883c27200cf56c6
Cris Thompson <cris@octophin.co.uk>
2019-10-28 15:16:12 +0000

    Adding client index and updating the version number.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='scute',
-      version='0.4.14',
+      version='0.4.15',
       description='Tool for making hardware user interfaces',
       url='https://github.com/Octophin/scute',
       author='Octophin Digital',
